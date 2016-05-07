677e7fbc6f8b60eace46acad7b5942fb0a7b8b6e
Kamal Ranaweera <ranaweer@ualberta.ca>
2016-05-07 01:05:29 -0600

    Creating the carousel dynamically.

--- home.js
+++ home.js
@@ -28,6 +28,14 @@ function showPageContents(pathToPage, displayDivId, cacheSeed){
 } //End: function showPageContents(paregnPageId, displayDivId)
 
 
+//function to convert date into a string
+//Intl.DateTimeFormat does a nice job but it fails for Safari 
+function Date2Str(date){
+	var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",  "November", "December"];
+	
+}
+
+
 //A utility function which shows posts from an announcement-type page in a specified display div
 function showAnnouncements(paregnPageId, displayDivId, panelHeading, cacheSeed){
 	var feed_url_base = "https://sites.google.com/feeds/content/"; 
@@ -115,4 +123,85 @@ function showAnnouncements(paregnPageId, displayDivId, panelHeading, cacheSeed){
 		}
 	});			
 } //End: function showAnnouncements(paregnPageId, displayDivId)
+
+
+//A utility function that shows images in a given page in the carousel
+function showCarouselImages(pathToDataPage, displayDivId, cacheSeed){
+	var feed_url_base = "https://sites.google.com/feeds/content/"; 
+	var feed_url = feed_url_base.concat(siteDomain, "/", siteName, "/?path=", pathToPage, "&t=", cacheSeed);
+
+	var feed = new google.feeds.Feed(feed_url);
+	feed.load(function(result) {
+		if (!result.error) {
+			if(result.feed.entries.length > 0){
+				var entry = result.feed.entries[0];
+				var container = document.getElementById(displayDivId);
+
+				//Creating cwrapper for arousel indicators
+				var ol = document.createElement("ol");
+				$(ol).addClass("carousel-indicators");
+				container.appendChild(ol);
+
+				//Creating wrapper for slides
+				var carousel_inner = document.createElement("div");
+				$(carousel_inner).attr("role", "listbox");
+				container.appendChild(carousel_inner);
+
+				//Creating left control
+				var left = document.createElement("a");
+				$(left).addClass("left");
+				$(left).addClass("carousel-control");
+				$(left).attr("href", displayDivId);
+				$(left).attr("role", "button");
+				$(left).attr("data-slide", "prev");
+				$(left).html("<span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span> <span class='sr-only'>Previous</span>");
+				container.appendChild(left);
+
+
+				//Creating right control
+				var right = document.createElement("a");
+				$(right).addClass("right");
+				$(right).addClass("carousel-control");
+				$(right).attr("href", displayDivId);
+				$(right).attr("role", "button");
+				$(right).attr("data-slide", "next");
+				$(right).html("<span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span> <span class='sr-only'>Next</span>");
+				container.appendChild(right);
+
+				//getting the first image, if any
+				var content = document.createElement("content");
+				content.innerHTML = entry.content;        
+				var images = $(content).find('img').map(function(){
+					return $(this).attr('src')
+				}).get();	
+
+				var i = 0;
+				foreach(var img in images){
+					//adding indicator
+					var li = document.createElement("li");
+					$(li).attr("data-target", displayDivId);
+					$(li).attr("data-slide-to", i);
+					if(i == 0){
+						$(right).addClass("active");
+					}
+					ol.appendChild(li);
+
+
+					//adding image
+					var img_div = document.createElement("div");
+					img_div.css("background-image", "url('".concat(img, "')"));
+					if(i == 0){
+						$(img_div).addClass("active");
+					}
+					carousel_inner.appendChild(img_div);
+
+					i = i + 1;
+				}				
+		}
+			gadgets.window.adjustHeight();
+		}
+	});			
+} //End: function showPageContents(paregnPageId, displayDivId)
+
+
 
