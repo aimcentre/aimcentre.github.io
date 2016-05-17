73dd88bb5af73d2c0198d7301eb6d7497e0fcc02
Kamal Ranaweera <kamal.ranaweera@ualberta.ca>
2016-05-16 22:32:22 -0600

    test

--- gallery.js
+++ gallery.js
@@ -60,9 +60,8 @@ function showAlbumCollectionByPath(year){
 	  	var entries = feed. getElementsByTagName("entry");
 	  	if(entries.length > 0){
 	  		var ids = entries[0].getElementsByTagName("id");
-			alert("ids: " + ids.length);
 	  		if(ids.length > 0){
-				
+				alert("ids.length: " + ids.length);
 		  		var url = $(ids[0]).text();
 		  		if(url.endsWith("/")){
 		  			url = url.slice(0,-1);
