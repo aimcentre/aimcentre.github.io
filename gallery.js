954e09c768b8a6ab815c5ceceff709c76d5386b8
Kamal Ranaweera <kamal.ranaweera@ualberta.ca>
2016-05-16 21:46:08 -0600

    test

--- gallery.js
+++ gallery.js
@@ -86,6 +86,8 @@ function showAlbumCollectionByParentPageId(parentId){
 			var id = "gallery-".concat(parentId);
 			var gallery_data = document.getElementById(id);
 			
+			alert('test');
+			
 			//if the gallery is not already loaded, load it.
 			if(gallery_data == null){
 				gallery_data = document.createElement("div");
