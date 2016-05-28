5434b95b7d96c90f25a5a73be4f0334f767bb310
Kamal Ranaweera <ranaweer@ualberta.ca>
2016-05-27 23:24:35 -0600

    update

--- abva_html.js
+++ abva_html.js
@@ -301,6 +301,7 @@ function showCalendarEvents(calendarId, apiKey, displayDivId, startTime, endTime
 			  //'timeZone=UTC-07%3A00&key=AIzaSyCTisDVkthQZRXOcQH1mu17gOscxM0R-Y4'
 
 	url = encodeURI(url);
+	url = url.replace(/:/g, "%3A");
 
 	$.ajax({
 	    type: 'GET',
