7783020c54195a101dccf573b40affc1207164a6
Kamal Ranaweera <kamal.ranaweera@ualberta.ca>
2016-05-29 01:54:34 -0600

    Update

--- abva_html.js
+++ abva_html.js
@@ -278,6 +278,8 @@ function showCarouselImages(pathToDataPage, displayDivId, cacheSeed, aspectRatio
 	});			
 } //End: function showPageContents(paregnPageId, displayDivId)
 
+function show(divId){$("#" + divId).show();}
+function hide(divId){$("#" + divId).hide();}
 function appendItemToFeed(title, description, shortDescLength, thumbnailUrl, fullPageUrl, type, start, end, targetDiv){
 	if(title == undefined)
 		return;
@@ -361,15 +363,17 @@ function appendItemToFeed(title, description, shortDescLength, thumbnailUrl, ful
 			var full_desc_div = document.createElement("div");
 			wrapper.appendChild(full_desc_div);
 			$(full_desc_div).hide();	
-			$(full_desc_div).html(description + " <a href = '#' onclick='return false;' >less</a>");
-			
-			$(snippet_div).html($(snippet_div).html() + " <a href = '#' onclick='return false;' >more</a>"); 
-			
+
 			var snippet_id = "sn_" + new Date().getTime() + Math.random().toString().substr(2,100);
 			$(snippet_div).attr("id", snippet_id);
 			
 			var full_desc_id = "fd_" + new Date().getTime() + Math.random().toString().substr(2,100);
 			$(full_desc_div).attr("id", full_desc_id);
+
+			$(full_desc_div).html(description + " <a href = '#' onclick='show(\"" + full_desc_div + "\"); return false;' >less</a>");
+			
+			$(snippet_div).html($(snippet_div).html() + " <a href = '#' onclick='return false;' >more</a>"); 
+			
 		}
 	}
 }
