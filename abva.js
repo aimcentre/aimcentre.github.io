4ce72cca5dbc44cff05c7f84abe0150f8697c937
Kamal Ranaweera <ranaweer@ualberta.ca>
2016-12-10 12:07:39 -0700

    Improvement
    showPageContents function now shows page title only if specifically
    requested

--- abva.js
+++ abva.js
@@ -1,6 +1,6 @@
 
 //A utility function which shows contents of a given page in a specified display div
-function showPageContents(pathToPage, displayDivId, cacheSeed){
+function showPageContents(pathToPage, displayDivId, cacheSeed, showPageTitle){
 	var feed_url_base = "https://sites.google.com/feeds/content/"; 
 	var feed_url = feed_url_base.concat(siteDomain, "/", siteName, "/?path=", pathToPage, "&t=", cacheSeed);
 
@@ -12,11 +12,12 @@ function showPageContents(pathToPage, displayDivId, cacheSeed){
 				var entry = result.feed.entries[0];
 				var container = document.getElementById(displayDivId);
 				
-				var h = document.createElement("h3");
-				h.className = "page-title";
-				h.innerHTML = entry.title;
-				container.appendChild(h);
-				
+				if(showPageTitle == true){
+					var h = document.createElement("h3");
+					h.className = "page-title";
+					h.innerHTML = entry.title;
+					container.appendChild(h);
+				}
 				var content = document.createElement("div");
 				content.innerHTML = entry.content;
 				container.appendChild(content);
